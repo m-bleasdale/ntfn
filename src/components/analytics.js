b29e2a299415f4b2a65add7beecf00ea12a90bad
LordMorgana <morganbleasdale234@gmail.com>
2025-07-09 12:26:51 +0100

    Banner fix

--- src/components/analytics.js
+++ src/components/analytics.js
@@ -6,12 +6,24 @@ export default function Analytics () {
     useEffect(() => {
         if (window.location.hostname === 'localhost') return;
 
+        const cookieBot = document.createElement('script');
+        cookieBot.id = 'Cookiebot';
+        cookieBot.src = 'https://consent.cookiebot.com/uc.js';
+        cookieBot.type = 'text/javascript';
+        cookieBot.setAttribute('data-cbid', 'd368d88e-41e7-4efe-ac83-8dd9b3ffcc2e');
+        cookieBot.setAttribute('data-blockingmode', 'auto');
+        cookieBot.async = true;
+        document.head.appendChild(cookieBot);
+
         const gaScript = document.createElement('script');
+        gaScript.setAttribute('data-cookieconsent', 'statistics');
         gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-4CBE8EKD6F';
         gaScript.async = true;
         document.head.appendChild(gaScript);
 
         const inlineScript = document.createElement('script');
+        inlineScript.type = 'text/plain';
+        inlineScript.setAttribute('data-cookieconsent', 'statistics');
         inlineScript.innerHTML = `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
@@ -19,14 +31,6 @@ export default function Analytics () {
             gtag('config', 'G-4CBE8EKD6F');
             `;
         document.head.appendChild(inlineScript);
-
-        const cookieBot = document.createElement('script');
-        cookieBot.id = 'Cookiebot';
-        cookieBot.src = 'https://consent.cookiebot.com/uc.js';
-        cookieBot.type = 'text/javascript';
-        cookieBot.setAttribute('data-cbid', 'd368d88e-41e7-4efe-ac83-8dd9b3ffcc2e');
-        cookieBot.async = true;
-        document.head.appendChild(cookieBot);
     }, []);
 
     return null;
