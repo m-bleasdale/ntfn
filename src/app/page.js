0fc08b92d84a4e75ef77e9430150e816fde63d02
LordMorgana <morganbleasdale234@gmail.com>
2025-07-13 18:49:42 +0100

    New article

--- src/app/page.js
+++ src/app/page.js
@@ -13,20 +13,16 @@ export default function Home() {
 		<div className="flex flex-col items-center min-h-screen pb-20 lg:gap-16 gap-8 p-8 sm:px-20 font-[family-name:var(--font-geist-sans)]">
 			<Header />
 			<main className="flex flex-col gap-[50px] items-center justify-center sm:items-start sm:w-full lg:w-[70%]">
-				<ArticlePreviewLarge path="what-went-wrong-with-kier-starmers-welfare-reform"/>
+				<ArticlePreviewLarge path="can-jury-free-trials-save-uk-courts"/>
 
-				{/*
 				<div className="grid lg:grid-rows-1 lg:grid-cols-3 sm:grid-rows-3 sm:grid-cols-1 gap-15">
-					<ArticlePreviewSmall path="2025/06/why-is-britain-broke"/>
-					<ArticlePreviewSmall path="2025/06/can-ukraine-win"/>
-					<ArticlePreviewSmall path="2025/06/what-israel-wants"/>
+					<ArticlePreviewSmall path="what-went-wrong-with-kier-starmers-welfare-reform"/>
 				</div>
-				*/}
 
 				<div className="w-full flex justify-center flex-col lg:gap-15 gap-12 lg:mb-20 mb-15">
 					<div className="flex flex-col gap-6">
 						<h1 className="lg:text-5xl text-3xl lg:mt-10 text-center font-semibold">More to Come</h1>
-						<p className="text-center font-medium text-muted-foreground">We only have one article at the moment, but are working on much more. Find out how you can help.</p>
+						<p className="text-center font-medium text-muted-foreground">We only have a few articles at the moment, but are working on much more. Find out how you can help.</p>
 						<div className="flex flex-col items-center justify-center gap-3 pt-3 pb-12">
 							<Button size="lg" asChild>
 								<a href="/get-involved">
--- src/components/image-wrapper.jsx
+++ src/components/image-wrapper.jsx
@@ -8,6 +8,7 @@ const creditLink = {
 	"CC BY-SA 4.0": <a className="underline" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>,
     "CC BY-SA 2.0": <a className="underline" href="https://creativecommons.org/licenses/by-sa/2.0/">CC BY-SA 2.0</a>,
     "CC 3.0": <a className="underline" href="https://creativecommons.org/licenses/by/3.0/">CC 3.0</a>,
+    "CC0 1.0 Universal": <a className="underline" href="https://creativecommons.org/publicdomain/zero/1.0/deed.en">CC0 1.0 Universal</a>,
 }
 
 export default function ImageWrapper ({children, credit, type}) {
