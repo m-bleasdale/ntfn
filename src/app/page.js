2ad67a2ad804a8437f8a0a2f9293831be62480f9
LordMorgana <morganbleasdale234@gmail.com>
2025-08-04 14:14:51 +0100

    New article - online safety act

--- src/app/page.js
+++ src/app/page.js
@@ -31,13 +31,12 @@ export default function Home() {
 		<div className="flex flex-col items-center min-h-screen pb-20 lg:gap-16 gap-8 p-8 sm:px-20 font-[family-name:var(--font-geist-sans)]">
 			<Header />
 			<main className="flex flex-col gap-[50px] items-center justify-center sm:items-start sm:w-full lg:w-[95%] xl:w-[90%] 2xl:w-[70%]">
-				<ArticlePreviewLarge path="2025/08/trump-moves-nuclear-submarines"/>
-
+				<ArticlePreviewLarge path="britains-online-safety-act"/>
 
 				<div className="grid md:grid-rows-1 md:grid-cols-3 sm:grid-rows-3 sm:grid-cols-1 md:gap-10 gap-15">
-					<ArticlePreviewSmall path="2025/07/russia-earthquake"/>		
+					<ArticlePreviewSmall path="2025/08/trump-moves-nuclear-submarines"/>
+					<ArticlePreviewSmall path="2025/07/russia-earthquake"/>
 					<ArticlePreviewSmall path="2025/07/un-warns-of-gaza"/>
-					<ArticlePreviewSmall path="2025/07/trump-and-epstein"/>
 				</div>
 
 				<h2 className="text-5xl font-semibold">More News</h2>
@@ -45,12 +44,16 @@ export default function Home() {
 				<ArticlePreviewLarge path="can-jury-free-trials-save-uk-courts"/>
 
 				<div className="grid md:grid-rows-1 md:grid-cols-3 sm:grid-rows-3 sm:grid-cols-1 md:gap-10 gap-15">
+					<ArticlePreviewSmall path="2025/07/trump-and-epstein"/>
 					<ArticlePreviewSmall path="2025/07/syrian-sectarian-conflict"/>		
 					<ArticlePreviewSmall path="2025/07/afghan-data-leak"/>
-					<ArticlePreviewSmall path="2025/07/voting-age-16"/>
 				</div>
 
-				<ArticlePreviewLarge path="what-went-wrong-with-kier-starmers-welfare-reform"/>
+				<div className="grid md:grid-rows-1 md:grid-cols-3 sm:grid-rows-3 sm:grid-cols-1 md:gap-10 gap-15">
+					<ArticlePreviewSmall path="2025/07/voting-age-16"/>
+					<ArticlePreviewSmall path="what-went-wrong-with-kier-starmers-welfare-reform"/>
+					<ArticlePreviewSmall path="2025/07/uk-inflation-surge"/>
+				</div>
 
 				<div className="w-full flex justify-center flex-col lg:gap-15 gap-12 lg:mb-20 mb-15 lg:mt-20 mt-15">
 					<Community />
