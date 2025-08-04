aee0a41dabfca8df21cb8bf127928e635eb14687
LordMorgana <morganbleasdale234@gmail.com>
2025-08-04 14:20:15 +0100

    changed li spacing

--- src/components/MDXComponents.js
+++ src/components/MDXComponents.js
@@ -7,7 +7,7 @@ export const MDXComponents = {
 	p: (props) => <p className="mb-10 text-lg leading-relaxed" {...props} />,
 	ul: (props) => <ul className="mb-10 text-lg list-disc mx-10" {...props} />,
 	ol: (props) => <ul className="mb-10 text-lg list-decimal mx-10" {...props} />,
-	li: (props) => <li className="mb-8" {...props} />,
+	li: (props) => <li className="mb-[-10px]" {...props} />,
 	strong: (props) => <strong className="font-semibold" {...props} />,
 	a: (props) => <a className="underline text-blue-800" {...props} />,
 	Pointer: (props) => <Pointer {...props} />
